dd3969d83be1dadc942924fcaac8721d474fa9a5
MuhammadZain845 <muhammad.zain@devsinc.com>
2025-06-02 21:12:00 +0500

    Session 5 Routing and Toast Service

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: HomeComponent},
+    {
+        path: '',        // Empty path to match the root URL i.e, localhost:4200/
+                         // This is a lazy-loaded route, meaning the components will be loaded only when the user navigates to this path
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members',component: MemberListComponent},
+            {path: 'members/:id',component: MemberDetailComponent},
+            {path: 'lists',component: ListsComponent},    // i.e, localhost:4200/lists
+            {path: 'messages',component: MessagesComponent},
+        ]
+    }, 
+    {path: '**',component: HomeComponent, pathMatch: 'full'},
+];
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   private accountService = inject(AccountService);
   //usersFromHomeComponent = input.required<any>(); // using new way, not by tradational decorators
   //@Output() cancelRegister = new EventEmitter();  // old syntax for EventEmitter that uses decorators
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {}
 
@@ -22,8 +24,8 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
-    });
+      error: error => this.toastr.error(error.error)
+    })
   }
 
   cancel() {
